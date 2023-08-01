ab55c06e97dcaf4de7d1e61cb2df39e05b17cdcd
CodeAGenie <nwibeg@gmail.com>
2023-08-01 03:47:27 +0100

    update

--- app.js
+++ app.js
@@ -15,12 +15,12 @@ function calculateResults(e){
 
     // for the exact value of the amount entered in decimal
     const principal = parseFloat(amount.value);
-    const calculatedInterest = parseFloat(interest.value)/100/12
+    const calculatedInterest = parseFloat(interest.value)/100 / 12
     const calculatedPayments = parseFloat(years.value) * 12;
 
     // compute monthly payment
     const x = Math.pow(1+calculatedInterest , calculatedPayments);
-    const monthly = (principal * x * calculateInterest)/(x-1);
+    const monthly = (principal * x * calculatedInterest)/(x-1);
 
     if(isFinite(monthly)){
         monthlyPayment.value = monthly.toFixed(2);
@@ -30,9 +30,30 @@ function calculateResults(e){
     else{
         showError('Please check your numbers');
     }
-
-
-
-     
+ 
     e.preventDefault();
 }
+function showError(error){
+    // create a div
+    const errorDiv = document.createElement('div');
+    // get elments
+    const card = document.querySelector('.card');
+    const heading = document.querySelector('.heading');
+
+    // add a class
+    errorDiv.className = 'alert alert-danger';
+
+    // create text node and append to div
+    errorDiv.appendChild(document.createTextNode(error)); 
+
+    // to insert error above heading
+    card.insertBefore(errorDiv,heading);
+
+   //    Clear out error message after  secs;
+     setTimeout(clearError, 3000);
+
+}
+    // clear error
+    function clearError(){
+        document.querySelector('.alert').remove();
+    }
