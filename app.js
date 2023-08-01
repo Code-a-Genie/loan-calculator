e1c1e3b16e27c407681c94997cd942c847ff8020
CodeAGenie <nwibeg@gmail.com>
2023-08-01 02:57:00 +0100

    syncing js

--- app.js
+++ app.js
@@ -12,5 +12,27 @@ function calculateResults(e){
     const monthlyPayment = document.getElementById('monthly-payment')
     const totalPayment = document.getElementById('total-payment')
     const totalInterest = document.getElementById('total-interest')
+
+    // for the exact value of the amount entered in decimal
+    const principal = parseFloat(amount.value);
+    const calculatedInterest = parseFloat(interest.value)/100/12
+    const calculatedPayments = parseFloat(years.value) * 12;
+
+    // compute monthly payment
+    const x = Math.pow(1+calculatedInterest , calculatedPayments);
+    const monthly = (principal * x * calculateInterest)/(x-1);
+
+    if(isFinite(monthly)){
+        monthlyPayment.value = monthly.toFixed(2);
+        totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+        totalInterest.value = ((monthly * calculatedPayments)-principal).toFixed(2);
+    } 
+    else{
+        showError('Please check your numbers');
+    }
+
+
+
+     
     e.preventDefault();
 }
